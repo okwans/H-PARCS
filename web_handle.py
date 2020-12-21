25a7b31825ac6853ee086e71b590bb6be268d5a1
okwans <okwans@naver.com>
2020-12-21 09:52:00 +0900

    web control 파일 추가

---
+++ web_handle.py
@@ -0,0 +1,68 @@
+# -*- coding:utf-8 -*-
+
+import time
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
+import platform
+
+class HandleWeb:
+
+    options = Options()
+
+    def init_web_page(self, url, WebBrowser):
+        if(platform.system() == "Windows"):
+            chromDriver = "C:\chromedriver.exe"  # Windows
+        elif(platform.system() == "Darwin"):
+            chromDriver = '/Users/ksoh/PycharmProjects/chromedriver' # MAC
+        else:
+            # chromDriver = '/Users/ksoh/PycharmProjects/chromedriver' # MAC
+            chromDriver = 'chromedriver'  # Docker
+            self.options.add_argument('disable-gpu')
+            self.options.add_argument('--no-sandbox')
+            self.options.add_argument('--disable-dev-shm-usage')
+            self.options.add_argument('headless')
+
+        driver = webdriver.Chrome(executable_path=chromDriver, options=self.options)
+        driver.set_window_size(1440, 960)
+        driver.delete_all_cookies()
+        driver.get(url)
+        time.sleep(5)
+
+        WebBrowser.append(driver)
+        return True
+
+    def web_find_element(selfself, WebBrowser, element):
+        html = WebBrowser.page_source
+        soup = BeautifulSoup(html, 'html.parser')
+
+        td = soup.find(element)
+        print(td.get_text())
+        return td.get_text()
+
+    def web_find_point(selfself, WebBrowser, element):
+        html = WebBrowser.page_source
+        soup = BeautifulSoup(html, 'html.parser')
+
+        td = soup.find('span', {'class': element})
+        print(td.get_text())
+        return td.get_text()
+
+    def webdriver_shutdown(self, WebBrowser):
+        driver = WebBrowser[0]
+        driver.close()
+        driver.quit()
+
+"""    def get_web_page(self, url, WebBrowser):
+        driver = WebBrowser[0]
+        driver.set_window_size(1440, 960)
+        driver.delete_all_cookies()
+        driver.get(url)
+        time.sleep(5)
+        WebBrowser.append(driver)
+        return True"""
+
+handleweb = HandleWeb()
