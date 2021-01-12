e20163bcba9fca1ec962d9e2d97050b437f74b04
ksoh <ksoh@humaxdigital.com>
2021-01-12 19:45:16 +0900

    API & Selenium을 이용한 관리자 수동 입출차 기능 테스트 구현 완료

--- features/B.Admin/B01.parkingManagements.feature
+++ features/B.Admin/B01.parkingManagements.feature
@@ -29,50 +29,28 @@ Feature: 관리자_주차장_운영정보_확인
     Examples: API_Data
     | uid   | plateNumber | parkingTimes | inGateDate      | outGateDate      |
     | 3461  | 67나3566     | 6시간 13분    | 2020-08-15 12:27 | 2020-08-15 18:40 |
-    | 11890 | 경기90자6743  | 0시간 24분    | 2020-11-19 14:32 | 2020-11-19 14:56 |
+    #| 11890 | 경기90자6743  | 0시간 24분    | 2020-11-19 14:32 | 2020-11-19 14:56 |
     | 11883 | 89노0993     | 0시간 11분    | 2020-11-19 13:47 | 2020-11-19 13:58 |
     | 11872 | 07가0202     | 39시간 5분    | 2020-11-17 20:14 | 2020-11-19 11:19 |
     | 11865 | 67호9155     | 0시간 10분    | 2020-11-19 10:11 | 2020-11-19 10:21 |
 
 
   @B01.03.관리자_주차장_운영정보_확인
-  Scenario Outline: 주차장 관리 - 관리자 수동 입차 기능 동작 확인
+  Scenario Outline: 주차장 관리 - 관리자 수동 입차/출차 기능 동작 확인
     Given B01.03 - API Test를 위한 서버가 준비되어 있고 필요한 access Token 데이터를 전달 받았다.
-    When B01.03 - API 동작을 통해 특정 차량를 참고하여 수동 입차 기능을 이용 "<plateNumber>", "<inGateDate>"정보를 수정한다.
-    Then B01.03 - 해당 차량의 수정된 정보는 입력된 "<plateNumber>", "<inGateDate>"와 동일해야 한다.
-    And B01.03 - Test 완료 후, 입차된 차량 정보는 삭제 진행한다.
+    When B01.03 - API 동작을 통해 특정 차량를 등록하고 browser에서 수동 입차 기능을 이용 "<plateNumber>"와 어제 입차일로 정보를 수정한다.
+    And B01.03 - 입차 등록된 차량에 대해 browser에서 수동 출차 기능을 이용 오늘 출차일로 정보를 수정한다.
+    Then B01.03 - 해당 차량의 수정된 정보는 입력된 "<plateNumber>"와 동일해야 하며, 입출차 날짜는 어제/오늘로 설정되어야 한다.
+    And B01.03 - Test 완료 후, 등록한 차량 정보는 삭제 진행한다.
     Examples: API_Data
-    | plateNumber | inGateDate |
-    | 10호1111    | yesterday   |
-    #| 10호2222    | yesterday   |
-    #| 10호3333    | yesterday   |
-    #| 10호4444    | yesterday   |
-    #| 10호5555    | yesterday   |
-    #| 10호6666    | yesterday   |
-    #| 10호7777    | yesterday   |
-    #| 10호8888    | yesterday   |
-    #| 10호9999    | yesterday   |
-    #| 10호1818    | yesterday   |
-
-  @B01.04.관리자_주차장_운영정보_확인
-  Scenario Outline: 주차장 관리 - 관리자 수동 출차 기능 동작 확인
-    Given B01.04 - API Test를 위한 서버가 준비되어 있고 필요한 access Token 데이터를 전달 받았다.
-    And B01.04 - Test를 위해 "<plateNumber>"와 "<plateNumber>"를 이용, API를 통해 차량을 입차 시킨다.
-    When B01.04 - API 동작을 통해 특정 차량 "<uid>"를 이용하여 수동 출차 기능을 통해 "<plateNumber>", 다정보를 수정한다.
-    Then B01.04 - 해당 차량의 수정된 정보는 입력된 "<plateNumber>", "<outGateDate>"와 동일해야 한다.
-    And B01.04 - Test 완료 후, 입차된 차량 정보는 삭제 진행한다.
-    Examples: API_Data
-    # 수동 출차 기능은 오직 출차가 완료되지 않은 차량에서만 가능함.
-    | plateNumber | inGateDate | outGateDate |
-    | 10호1111    | yesterday   | today       |
-    | 10호2222    | yesterday   | today       |
-    | 10호3333    | yesterday   | today       |
-    | 10호4444    | yesterday   | today       |
-    | 10호5555    | yesterday   | today       |
-    | 10호6666    | yesterday   | today       |
-    | 10호7777    | yesterday   | today       |
-    | 10호8888    | yesterday   | today       |
-    | 10호9999    | yesterday   | today       |
-    | 10호1818    | yesterday   | today       |
-
-
+    | plateNumber |
+    | 10호1111    |
+    | 10호2222    |
+    | 10호3333    |
+    | 10호4444    |
+    | 10호5555    |
+    | 10호6666    |
+    | 10호7777    |
+    | 10호8888    |
+    | 10호9999    |
+    | 10호1818    |
--- features/steps/B01_parkingManagements.py
+++ features/steps/B01_parkingManagements.py
@@ -202,7 +202,7 @@ def step_impl(context, plateNumber, parkingTimes, inGateDate, outGateDate):
 
 # #####################################################################################################################
 # @B01.03.관리자_주차장_운영정보_확인
-#   Scenario Outline: 주차장 관리 - 관리자 수동 입차 기능 동작 확인
+#   Scenario Outline: 주차장 관리 - 관리자 수동 입차/출차 기능 동작 확인
 @given('B01.03 - API Test를 위한 서버가 준비되어 있고 필요한 access Token 데이터를 전달 받았다.')
 def step_impl(context):
     try:
@@ -236,206 +236,266 @@ def step_impl(context):
     assert (response_data.status_code == 200)
 
 
-@when('B01.03 - API 동작을 통해 특정 차량를 참고하여 수동 입차 기능을 이용 "{plateNumber}", "{inGateDate}"정보를 수정한다.')
-def step_impl(context, plateNumber, inGateDate):
-    #try:
-    global Test_Uid, CarOverTime, CarEntryTime
-
-    headers = {
-        "Authorization": "Bearer " + access_Token
-    }
-
-    data = {
-        "machineCode": "PM0001",
-        "plateNumber": plateNumber,
-        "cameraType": "1",
-        "eventId": "0",
-        "plateImg": "test" # 별도 이미지는 첨부하지 않음
-    }
-
-    # 먼저 /gate/vehicle/in을 이용하여 차량 입차 동작을 수행
-    Test_URL = api + "/gate/vehicle/in"
-    response_data = requests.post(Test_URL, headers=headers, data=data)
-    VehicleIn = json.loads(response_data.text)
-    # print(response_data.content)
-
-    print(">>> 차량 입차 >>>>>>>>>>>>>>>>")
-    oks = json.dumps(VehicleIn, indent=4, sort_keys=False, ensure_ascii=False)
-    print(oks)
-
-    if (response_data.status_code == 200):
-        print("### status-code : " + str(response_data.status_code))
-        print("### response data OK!")
-        Test_Result = True
-    else:
-        print("### status-code : " + str(response_data.status_code))
-        print("### response data Not OK!")
-        raise Exception("### response data Not OK!")
-
-    # 입차 동작을 수행한 차량의 plateNumber를 이용하여 uid를 확인
-    Test_URL = api + "/parkings"
-    response_data = requests.get(Test_URL, headers=headers)
-    All_vehivle = json.loads(response_data.text)
-
-    # match되는 plateNumber가 있을 경우 해당 uid를 확인
-    for temp in All_vehivle['data']['rows']:
-        if temp['plateNumber'] == plateNumber:
-            Test_Uid = temp['uid']
-            print(Test_Uid)
-            break
-
-    # 확인된 uid를 이용하여 해당 차량의 inGateDate 변경
-    Test_URL2 = api + "/parkings/" + str(Test_Uid)
-
-    # 해당 차량의 입차 시간을 Null 값을 이용하여 미인식차량으로 변
-    headers = {
-        "Authorization": "Bearer " + access_Token,
-        "Content-Type": "application/json"
-    }
-
-    data = {
-        "inGateDate": None
-    }
-
-    response_data = requests.put(Test_URL2, headers=headers, data=json.dumps(data))
-    print("### status-code : " + str(response_data.status_code))
-    VehicleIn = json.loads(response_data.text)
-    print(">>> 차량 입차 시간 수정 >>>>>>>>>>>>>>>>")
-    oks = json.dumps(VehicleIn, indent=4, sort_keys=False, ensure_ascii=False)
-    print(oks)
-
-    # 미인식차량에 WebBrowser를 통해 입차 시간을 현재시간 기준 어제로 변경한다.
-    web_url = "http://localhost:3000"
-    handleweb.init_web_page(web_url, WebBrowser)
-
-    # Browser에서 주차관리 버튼 click
-    driver = WebBrowser[0]
-    parkingManagement = driver.find_element_by_xpath('//*[@id="sc-header"]/nav/div[2]/div/a[2]/button/span')
-    parkingManagement.click()
-    time.sleep(2)
-
-    # 차량 List에서 방금 입차 동작을 수행한 항목을 click
-    targetCar = driver.find_element_by_xpath('//*[@id="sc-page-content"]/div[1]/div[1]/div/div[2]/div[2]/div/div/div[1]/div/div[3]/div[2]/div/div/div[1]/div[2]/span/span')
-    targetCar.click()
-    time.sleep(2)
-
-    # 수동 입차 버튼 click
-    manual_in = driver.find_element_by_xpath('//*[@id="sc-page-content"]/div[1]/div[2]/div/div/div[2]/div[2]/div[2]/div[2]/button[1]')
-    manual_in.click()
-    time.sleep(2)
-
-    # 입차일시 선택
-    inGatedateButton = driver.find_element_by_xpath('//*[@id="modal-manual-invehicle"]/div/div[2]/div[1]/div/input')
-    inGatedateButton.click()
-    time.sleep(2)
-
-    # 표시되는 달력에서 어제 날짜를 찾아 선택한다.
-    today = datetime.datetime.today()
-
-    # 표시되는 달력에서 어제 날짜를 찾아 선택하고, 매월 첫번째 날일 경우 그전달 마지막일로 변경
-    if int(today.day) == 1:
-        if today.month == 1:
-            targetDay = calendar.monthrange(int(today.year - 1), 12)[1]
-            refDate = "12월 " + str(targetDay) + ", " + str(today.year - 1)
-        else:
-            targetDay = calendar.monthrange(today.year, int(today.month - 1))[1]
-            refDate = str(today.month - 1) + "월 " + str(targetDay) + ", " + str(today.year)
-
-        beforeMonth = driver.find_element_by_xpath('/html/body/div[4]/div[1]/span[1]')
-        beforeMonth.click()
-        time.sleep(3)
-    else:
-        targetDay = today.day - 1
-        refDate = str(today.month) + "월 " + str(targetDay) + ", " + str(today.year)
-
-    print("000 >>>>" + refDate)
-    #aria - label = "12월 1, 2020"
-    #findTargetDay = driver.find_elements_by_class_name('flatpickr-day').get_attribute('aria-label')
-    #findTargetDay = driver.find_elements_by_tag_name('span')
-
-    selectDay = driver.find_element_by_xpath('/html/body/div[4]/div[2]/div/div[2]/div/span[contains(@aria-label, "' + refDate + '")]')
-    selectDay.click()
-
-    # 확인 버튼 click
-    confirmButton = driver.find_element_by_xpath('/html/body/div[4]/div[4]')
-    confirmButton.click()
-    time.sleep(4)
-
-    # 저장 버튼 click
-    saveButton = driver.find_element_by_xpath('//*[@id="modal-manual-invehicle"]/div/div[2]/div[2]/button')
-    saveButton.click()
-    time.sleep(2)
-
-    #except Exception as e:
-    #    print(e)
-    #    Test_Result = False
-
-    assert True
-
-
-@then('B01.03 - 해당 차량의 수정된 정보는 입력된 "{plateNumber}", "{inGateDate}"와 동일해야 한다.')
-def step_impl(context, plateNumber, inGateDate):
-    #try:
-
-    #except Exception as e:
-    #    print(e)
-    #    Test_Result = False
-
-    assert True
-
-
-# #####################################################################################################################
-# @B01.04.관리자_주차장_운영정보_확인
-#   Scenario Outline: 주차장 관리 - 관리자 수동 출차 기능 동작 확인
-@given('B01.04 - API Test를 위한 서버가 준비되어 있고 필요한 access Token 데이터를 전달 받았다.')
-def step_impl(context):
+@when('B01.03 - API 동작을 통해 특정 차량를 등록하고 browser에서 수동 입차 기능을 이용 "{plateNumber}"와 어제 입차일로 정보를 수정한다.')
+def step_impl(context, plateNumber):
     try:
-        # API 동작에 필요한 Token 설정 진행 #################################################
-        global access_Token
-
-        Token_URL = api + "/auth/login"
-        print('User Token url = ' + Token_URL)
+        global Test_Uid
 
         headers = {
-            'Content-Type': 'application/json'
+            "Authorization": "Bearer " + access_Token
         }
 
         data = {
-            "id": ID,
-            "password": PW
+            "machineCode": "PM0001",
+            "plateNumber": plateNumber,
+            "cameraType": "1",
+            "eventId": "0",
+            "plateImg": "test" # 별도 이미지는 첨부하지 않음
         }
 
-        response_data = requests.post(Token_URL, headers=headers, data=json.dumps(data))
-        print(response_data.status_code)
+        # 먼저 /gate/vehicle/in을 이용하여 차량 입차 동작을 수행
+        Test_URL = api + "/gate/vehicle/in"
+        response_data = requests.post(Test_URL, headers=headers, data=data)
+        VehicleIn = json.loads(response_data.text)
+        # print(response_data.content)
 
-        response_json = json.loads(response_data.text)
-        access_Token = response_json['data']['token']
-        print(ID + " - accessToken >>> " + access_Token)
-        resp = json.dumps(response_json, indent=4, sort_keys=False, ensure_ascii=False)
-        print(resp)
+        print(">>> 차량 입차 >>>>>>>>>>>>>>>>")
+        oks = json.dumps(VehicleIn, indent=4, sort_keys=False, ensure_ascii=False)
+        print(oks)
+
+        if (response_data.status_code == 200):
+            print("### status-code : " + str(response_data.status_code))
+            print("### response data OK!")
+            Test_Result = True
+        else:
+            print("### status-code : " + str(response_data.status_code))
+            print("### response data Not OK!")
+            raise Exception("### response data Not OK!")
+
+        # 입차 동작을 수행한 차량의 plateNumber를 이용하여 uid를 확인
+        Test_URL = api + "/parkings"
+        response_data = requests.get(Test_URL, headers=headers)
+        All_vehivle = json.loads(response_data.text)
+
+        # match되는 plateNumber가 있을 경우 해당 uid를 확인
+        for temp in All_vehivle['data']['rows']:
+            if temp['plateNumber'] == plateNumber:
+                Test_Uid = temp['uid']
+                print(Test_Uid)
+                break
+
+        # 확인된 uid를 이용하여 해당 차량의 inGateDate 변경
+        Test_URL2 = api + "/parkings/" + str(Test_Uid)
+
+        # 해당 차량의 입차 시간을 Null 값을 이용하여 미인식차량으로 변경
+        headers = {
+            "Authorization": "Bearer " + access_Token,
+            "Content-Type": "application/json"
+        }
+
+        data = {
+            "inGateDate": None
+        }
+
+        response_data = requests.put(Test_URL2, headers=headers, data=json.dumps(data))
+        print("### status-code : " + str(response_data.status_code))
+        VehicleIn = json.loads(response_data.text)
+        print(">>> 차량 입차 시간 수정 >>>>>>>>>>>>>>>>")
+        oks = json.dumps(VehicleIn, indent=4, sort_keys=False, ensure_ascii=False)
+        print(oks)
+
+        # 미인식차량에 WebBrowser를 통해 입차 시간을 현재시간 기준 어제로 변경한다.
+        web_url = "http://localhost:3000"
+        handleweb.init_web_page(web_url, WebBrowser)
+
+        # Browser에서 주차관리 버튼 click
+        driver = WebBrowser[0]
+        parkingManagement = driver.find_element_by_xpath('//*[@id="sc-header"]/nav/div[2]/div/a[2]/button/span')
+        parkingManagement.click()
+        time.sleep(2)
+
+        # 차량 List에서 방금 입차 동작을 수행한 항목을 click
+        targetCar = driver.find_element_by_xpath('//*[@id="sc-page-content"]/div[1]/div[1]/div/div[2]/div[2]/div/div/div[1]/div/div[3]/div[2]/div/div/div[1]/div[2]/span/span')
+        targetCar.click()
+        time.sleep(2)
+
+        # 수동 입차 버튼 click
+        manual_in = driver.find_element_by_xpath('//*[@id="sc-page-content"]/div[1]/div[2]/div/div/div[2]/div[2]/div[2]/div[2]/button[1]')
+        manual_in.click()
+        time.sleep(2)
+
+        # 입차일시 선택
+        inGatedateButton = driver.find_element_by_xpath('//*[@id="modal-manual-invehicle"]/div/div[2]/div[1]/div/input')
+        inGatedateButton.click()
+        time.sleep(2)
+
+        # 표시되는 달력에서 어제 날짜를 찾아 선택한다.
+        today = datetime.datetime.today()
+
+        # 표시되는 달력에서 어제 날짜를 찾아 선택하고, 매월 첫번째 날일 경우 그전달 마지막일로 변경
+        if int(today.day) == 1:
+            if today.month == 1:
+                targetDay = calendar.monthrange(int(today.year - 1), 12)[1]
+                refDate = "12월 " + str(targetDay) + ", " + str(today.year - 1)
+            else:
+                targetDay = calendar.monthrange(today.year, int(today.month - 1))[1]
+                refDate = str(today.month - 1) + "월 " + str(targetDay) + ", " + str(today.year)
+
+            beforeMonth = driver.find_element_by_xpath('/html/body/div[4]/div[1]/span[1]')
+            beforeMonth.click()
+            time.sleep(3)
+        else:
+            targetDay = today.day - 1
+            refDate = str(today.month) + "월 " + str(targetDay) + ", " + str(today.year)
+
+        print("000 >>>>" + refDate)
+        #aria - label = "12월 1, 2020"
+        #findTargetDay = driver.find_elements_by_class_name('flatpickr-day').get_attribute('aria-label')
+        #findTargetDay = driver.find_elements_by_tag_name('span')
+
+        selectDay = driver.find_element_by_xpath('/html/body/div[4]/div[2]/div/div[2]/div/span[contains(@aria-label, "' + refDate + '")]')
+        selectDay.click()
+
+        # 확인 버튼 click
+        confirmButton = driver.find_element_by_xpath('/html/body/div[4]/div[4]')
+        confirmButton.click()
+        time.sleep(4)
+
+        # 저장 버튼 click
+        saveButton = driver.find_element_by_xpath('//*[@id="modal-manual-invehicle"]/div/div[2]/div[2]/button')
+        saveButton.click()
+        time.sleep(2)
+
+        # browser 종료
+        handleweb.webdriver_shutdown(WebBrowser)
+
+        Test_Result = True
 
     except Exception as e:
         print(e)
+        Test_Result = False
 
-    assert (response_data.status_code == 200)
+    assert Test_Result
 
 
-@when('B01.04 - API 동작을 통해 특정 차량 "{uid}"를 이용하여 수동 출차 기능을 통해 "{plateNumber}", "{exitTime}"정보를 수정한다.')
-def step_impl(context, uid, plateNumber, exitTime):
+@when('B01.03 - 입차 등록된 차량에 대해 browser에서 수동 출차 기능을 이용 오늘 출차일로 정보를 수정한다.')
+def step_impl(context):
     try:
-        print("E")
+        # 미인식차량에 WebBrowser를 통해 입차 시간을 현재시간 기준으로 변경한다.
+        web_url = "http://localhost:3000"
+        handleweb.init_web_page(web_url, WebBrowser)
+
+        # Browser에서 주차관리 버튼 click
+        driver = WebBrowser[0]
+        parkingManagement = driver.find_element_by_xpath('//*[@id="sc-header"]/nav/div[2]/div/a[2]/button/span')
+        parkingManagement.click()
+        time.sleep(2)
+
+        # 차량 List에서 방금 수동 입차 동작을 수행한 항목을 click
+        targetCar = driver.find_element_by_xpath(
+            '//*[@id="sc-page-content"]/div[1]/div[1]/div/div[2]/div[2]/div/div/div[1]/div/div[3]/div[2]/div/div/div[1]/div[2]/span/span')
+        targetCar.click()
+        time.sleep(2)
+
+        # 수동 출차 버튼 click
+        manual_in = driver.find_element_by_xpath(
+            '//*[@id="sc-page-content"]/div[1]/div[2]/div/div/div[2]/div[2]/div[2]/div[2]/button[2]/div')
+        manual_in.click()
+        time.sleep(2)
+
+        # 확인 버튼 click
+        confirmButton = driver.find_element_by_xpath('//*[@id="modal-manual-outvehicle"]/div/div/div[2]/button[2]')
+        confirmButton.click()
+        time.sleep(4)
+
+        Test_Result = True
+
     except Exception as e:
         print(e)
+        Test_Result = False
 
-    assert True
+    assert Test_Result
 
 
-@then('B01.04 - 해당 차량의 수정된 정보는 입력된 "{plateNumber}", "{exitTime}"와 동일해야 한다.')
-def step_impl(context, plateNumber, exitTime):
-    # try:
+@then('B01.03 - 해당 차량의 수정된 정보는 입력된 "{plateNumber}"와 동일해야 하며, 입출차 날짜는 어제/오늘로 설정되어야 한다.')
+def step_impl(context, plateNumber):
+    try:
 
-    # except Exception as e:
-    #    print(e)
-    #    Test_Result = False
+        headers = {
+            "Authorization": "Bearer " + access_Token
+        }
 
-    assert True
+        target_URL = "http://localhost:3000/api/parkings/" + str(Test_Uid)
+        response_data = requests.get(target_URL, headers=headers)
+        print(response_data.status_code)
+        response_json = json.loads(response_data.text)
+        #resp = json.dumps(response_json, indent=4, sort_keys=False, ensure_ascii=False)
+        #print(resp)
+
+        temp_ref_outGateDate = response_json['data']['outGateDate']
+        temp_ref_inGateDate = response_json['data']['inGateDate']
+
+        ref_plateNumber = response_json['data']['plateNumber']
+        ref_inGateDate = temp_ref_inGateDate.split(' ') # 입차 시간은 날짜만 확인
+        ref_outGateDate = temp_ref_outGateDate.split(' ') # 출차 시간은 날짜만 확인
+
+        # 저장된 data의 확인을 위해 실제 어제 날짜와 오늘 날짜를 확인
+        today = datetime.datetime.today()
+
+        # 표시되는 달력에서 어제 날짜를 찾아 선택하고, 매월 첫번째 날일 경우 그전달 마지막일로 변경
+        if int(today.day) == 1:
+            if today.month == 1:
+                targetDay = calendar.monthrange(int(today.year - 1), 12)[1]
+                date_yesterday = str(today.year - 1) + "-" + "12" + "-" + '{0:0>2}'.format(str(targetDay))
+            else:
+                targetDay = calendar.monthrange(today.year, int(today.month - 1))[1]
+                date_yesterday = str(today.year) + "-" + '{0:0>2}'.format(str(today.month - 1)) + "-" + '{0:0>2}'.format(str(targetDay))
+        else:
+            targetDay = today.day - 1
+            date_yesterday = str(today.year) + "-" + '{0:0>2}'.format(str(today.month)) + "-" + '{0:0>2}'.format(str(targetDay))
+
+        date_today = str(today.year) + "-" + '{0:0>2}'.format(str(today.month)) + "-" + '{0:0>2}'.format(str(today.day))
+
+        print("Today >>> " + date_today)
+        print("Yesterday >>> " + date_yesterday)
+        print("Data_Yesterday >>> " + ref_inGateDate[0])
+        print("Data_Today >>> " + ref_outGateDate[0])
+
+        if plateNumber == ref_plateNumber and date_yesterday == ref_inGateDate[0] and date_today == ref_outGateDate[0]:
+            print("### Test Pass ###")
+            Test_Result = True
+        else:
+            print("### Test Fail ###")
+            Test_Result = False
+            resp = json.dumps(response_json, indent=4, sort_keys=False, ensure_ascii=False)
+            print(resp)
+
+        # browser 종료
+        handleweb.webdriver_shutdown(WebBrowser)
+
+    except Exception as e:
+        print(e)
+        Test_Result = False
+
+    assert Test_Result
+
+
+@then('B01.03 - Test 완료 후, 등록한 차량 정보는 삭제 진행한다.')
+def step_impl(context):
+    try:
+        headers = {
+            "Authorization": "Bearer " + access_Token
+        }
+
+        test_URL = "http://localhost:3000/api/parkings/" + str(Test_Uid)
+        response_data = requests.delete(test_URL, headers=headers)
+        print(response_data.status_code)
+        print("### Delete >>> " + str(Test_Uid))
+
+        Test_Result = True
+
+    except Exception as e:
+       print(e)
+       Test_Result = False
+
+    assert Test_Result
